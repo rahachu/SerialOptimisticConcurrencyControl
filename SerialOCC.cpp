d9b55d5a9243405f1d7e61a7151ebe7be74e7de0
rahachu <49639890+rahachu@users.noreply.github.com>
2020-11-13 22:34:55 +0700

    Add files via upload

--- SerialOCC.cpp
+++ SerialOCC.cpp
@@ -5,15 +5,23 @@ int main(int argc, char const *argv[])
 {
     /* code */
     int a,b;
+    cout << "T1 start" << endl;
     Transaction t1;
+    cout << "T1 read variable b" << endl;
     t1.read(&b);
+    cout << "T2 start" << endl;
     Transaction t2;
+    cout << "T2 read variable b" << endl;
     t2.read(&b);
+    cout << "T2 read variable a" << endl;
     t2.read(&a);
-    t1.validate();
+    cout << "T1 validate" << endl;
     #ifdef testcase2
+    cout << "T1 write variable a" << endl;
     t1.write(&a,5);
     #endif
+    t1.validate();
+    cout << "T2 validate" << endl;
     t2.validate();
     return 0;
 }
